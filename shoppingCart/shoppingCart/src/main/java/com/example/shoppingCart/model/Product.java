f6ea832ccb1b036e354ed01864f09f0f0f642a33
Efrain-Rivero <erive193@fiu.edu>
2023-11-06 20:34:45 -0500

    Create Product.java

---
+++ shoppingCart/shoppingCart/src/main/java/com/example/shoppingCart/model/Product.java
@@ -0,0 +1,45 @@
+package com.example.shoppingCart.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("products")
+public class Product {
+
+    @Id
+    private String id;
+    private String userID;
+    private String bookID;
+
+    public Product() {
+    }
+
+    public Product(String userID, String bookID) {
+        this.userID = userID;
+        this.bookID = bookID;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getBookID() {
+        return bookID;
+    }
+
+    public void setBookID(String bookID) {
+        this.bookID = bookID;
+    }
+}
